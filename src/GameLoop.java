f83627a275f24a6f27634fb042dec147ad5be502
Ben Graham <benwilliamgraham@gmail.com>
2019-02-09 13:27:13 -0500

    Save

--- src/GameLoop.java
+++ src/GameLoop.java
@@ -16,15 +16,14 @@ public class GameLoop {
 		
 		//create shaders
 		ShaderProgram shaderProgram = new ShaderProgram("src/shader/3d.vertex", "src/shader/3d.fragment");
+		ShaderProgram GUIProgram = new ShaderProgram("src/shader/2d.vertex", "src/shader/2d.fragment");
 		
 		//run gameloop
 		double lastTime = glfwGetTime();
 				
 		while (!glfwWindowShouldClose(window.id)) {
-<<<<<<< HEAD
+			//AudioPlayer.hit.play();
 			
-=======
->>>>>>> parent of 400faec... GUI ready for filming
 			//check events
 			glfwPollEvents();
 			
@@ -35,7 +34,7 @@ public class GameLoop {
 			map.update(window, client);
 			
 			//draw
-			map.render(map.activeCamera, shaderProgram);
+			map.render(map.activeCamera, shaderProgram, GUIProgram);
 			
 			//show
 			glfwSwapBuffers(window.id);
